0e8e6b06e6fadeb8072673ca69ac40710fbea02b
yuchaoqian <yuchao.qian@efrei.net>
2017-08-11 19:40:54 +0200

    fix bugs

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mysql = require('mysql2/promise');
 var mysqlFunction = require('mysql2');
 var corsOptions = {
   origin: true,
-  method:['GET', 'PUT', 'POST'],
+  method:['GET', 'PUT', 'POST', 'DELETE'],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 var getConnection = function(){
