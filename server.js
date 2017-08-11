8bfdff755872066606306c0229aac12ba19ffd3a
yuchaoqian <yuchao.qian@efrei.net>
2017-08-11 14:11:01 +0200

    modify utilisateur

--- server.js
+++ server.js
@@ -35,13 +35,63 @@ app.use(cors(corsOptions));
 app.get('/contrats', function (req, res) {
   queryDB(res,'select * from contrats');
 })
+//data base : users
+//get user data
 app.get('/utilisateurs', function (req, res) {
   queryDB(res,'SELECT * FROM `users`');
 })
+//delete user data
+app.delete('/utilisateurs', jsonParser, function(req,res){
+  let data = req.body;
+  console.log("data",data);
+  let queryString = 'DELETE FROM `users` WHERE `users`.`id_user` = \''+ data["id_user"]+'\';';
+  console.log("queryString",queryString);
+  queryDB(res,queryString);
+})
+//add new user
+app.post('/utilisateurs',jsonParser, function (req, res) {
+  let data = req.body;
+  let timeString = new Date().toJSON().split('T')[0];
+  let statut = 1;
+  console.log("data",data);
+  let queryString = 'INSERT INTO `users` (`id_user`, `nom`, `prenom`, `societe`, `username`, `password`, `email`, `tel`, `statut`, `profil`, `dateajout`) VALUES '
+  let params = '('+'NULL'+',\''+
+  data["nom"]+'\''+',\''+
+  data["prenom"]+'\''+',\''+
+  data["societe"]+'\''+',\''+
+  data["username"]+'\''+',\''+
+  data["motdepasse"]+'\''+',\''+
+  data["email"]+'\''+',\''+
+  data["tel"]+'\''+','+
+  statut+','+
+  data["profil"]+',\''+
+  timeString+'\''+')';
+  queryString = queryString.concat(params);
+  console.log("queryString",queryString);
+  queryDB(res,queryString);
+})
+//modify user data
+app.put('/utilisateurs',jsonParser, function (req, res) {
+  let data = req.body;
+  console.log("data",data);
+  let queryString = 'UPDATE `users` SET `nom` = '
+  +'\''+data["nom"]+'\''+', `prenom` = '
+  +'\''+data["prenom"]+'\''+', `societe` = '
+  +'\''+data["societe"]+'\''+', `username` = '
+  +'\''+data["username"]+'\''+', `password` = '
+  +'\''+data["password"]+'\''+', `email` = '
+  +'\''+data["email"]+'\''+', `tel` = '
+  +'\''+data["tel"]+'\''+', `profil` = '
+  +'\''+data["profil"]+'\''+' WHERE `users`.`id_user` = '+data["id_user"];
+  console.log("queryString",queryString);
+  queryDB(res,queryString);
+})
+//Profils
+//get all profil
 app.get('/profils', function (req, res) {
   queryDB(res,'SELECT * FROM `profils`');
 })
-//Profils
+//delete profil by id
 app.delete('/profils', jsonParser, function(req,res){
   let data = req.body;
   console.log("data",data);
@@ -49,6 +99,7 @@ app.delete('/profils', jsonParser, function(req,res){
   console.log("queryString",queryString);
   queryDB(res,queryString);
 })
+//updata a profil
 app.put('/profils', jsonParser, function(req,res){
   let data = req.body;
   console.log("data",data);
@@ -56,6 +107,7 @@ app.put('/profils', jsonParser, function(req,res){
   console.log("queryString",queryString);
   queryDB(res,queryString);
 })
+//add a new profil
 app.post('/profils', jsonParser, function(req,res){
   let data = req.body;
   console.log("data",data);
